41bd8b5031fbe4b212d5694c78b31ca24c807206
CodeSDK <codesdktutorials@gmail.com>
2019-01-03 22:01:34 -0500

    Centered Button

--- zumbastore/zumbastore/src/components/header.js
+++ zumbastore/zumbastore/src/components/header.js
@@ -1,21 +1,26 @@
 
 import React, { Component } from 'react';
-import { Container, Button, Text } from 'native-base';
+import { Container, Button, Text, View } from 'native-base';
 
 export default class Header extends Component {
         render() {
-                const { textStyle } = styles;
+                const { textStyle, viewStyle } = styles;
                 return (
+                        <View style={viewStyle}>
                                 <Button>
                                         <Text style={textStyle}>
                                                 Button
                                         </Text>
                                 </Button>
+                        </View>
                 );
         }
 }
 
 const styles = {
+        viewStyle: {
+                backgroundColor: '#F8F8F8'
+        },
         textStyle: {
                 fontSize: 20
         }
